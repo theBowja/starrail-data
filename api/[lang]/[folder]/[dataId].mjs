b9ca0badfdc792de12302763b7fd614601e1e1fe
theBowja <theBowja@users.noreply.github.com>
2023-10-17 08:39:47 -0400

    Fix cors

--- api/[lang]/[folder]/[dataId].mjs
+++ api/[lang]/[folder]/[dataId].mjs
@@ -1,8 +1,26 @@
 import { getDataItem } from '../../../main.js';
 
 export default function handler(request, response) {
+	if (!enableCors(request, response)) return;
 	const { lang, folder, dataId } = request.query;
 	
 	const result = getDataItem(lang, folder, dataId);
 	return response.json(result);
+}
+
+function enableCors(req, res) {
+	res.setHeader('Access-Control-Allow-Credentials', true)
+	res.setHeader('Access-Control-Allow-Origin', '*')
+	// another common pattern
+	// res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+	res.setHeader('Access-Control-Allow-Methods', 'GET')
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'folder-type, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+	)
+	if (req.method === 'OPTIONS') {
+		res.status(200).end();
+	return false;
+	}
+	return true;
 }
