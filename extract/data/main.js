11f64e4db248ae0e727aef21c542895bd93f865c
theBowja <ericxin@gmail.com>
2024-02-07 11:09:48 -0500

    no changes

--- extract/data/main.js
+++ extract/data/main.js
@@ -5,30 +5,30 @@ function exportStarRailData() {
 
 	setVersion('2.0');
 
-	// exportData('characters', require('./extractfolders/extractCharacters'));
-	// exportData('characterskills', require('./extractfolders/extractCharacterSkills'));
-	// exportData('charactertraces', require('./extractfolders/extractCharacterTraces'));
-	// exportData('lightcones', require('./extractfolders/extractLightCones'));
-	// exportData('relicsets', require('./extractfolders/extractRelicSets'));
+	exportData('characters', require('./extractfolders/extractCharacters'));
+	exportData('characterskills', require('./extractfolders/extractCharacterSkills'));
+	exportData('charactertraces', require('./extractfolders/extractCharacterTraces'));
+	exportData('lightcones', require('./extractfolders/extractLightCones'));
+	exportData('relicsets', require('./extractfolders/extractRelicSets'));
 
-	// exportData('elements', require('./extractfolders/extractElements'));
-	// exportData('paths', require('./extractfolders/extractPaths'));
+	exportData('elements', require('./extractfolders/extractElements'));
+	exportData('paths', require('./extractfolders/extractPaths'));
 
-	// exportData('enemies', require('./extractfolders/extractEnemies'));
+	exportData('enemies', require('./extractfolders/extractEnemies'));
 
-	// exportData('warps', require('./extractfolders/extractWarps'));
-	// exportData('travellogs', require('./extractfolders/extractTravelLogs'));
-	// exportData('trailblazelevels', require('./extractfolders/extractTrailblazeLevels'));
-	// exportData('achievements', require('./extractfolders/extractAchievements'));
-	// // exportData('items', require('./extractfolders/extractItems'));
+	exportData('warps', require('./extractfolders/extractWarps'));
+	exportData('travellogs', require('./extractfolders/extractTravelLogs'));
+	exportData('trailblazelevels', require('./extractfolders/extractTrailblazeLevels'));
+	exportData('achievements', require('./extractfolders/extractAchievements'));
+	// exportData('items', require('./extractfolders/extractItems'));
 
-	// exportData('loadingtips', require('./extractfolders/extractLoadingTips'));
+	exportData('loadingtips', require('./extractfolders/extractLoadingTips'));
 
-	// exportData('messagecontacts', require('./extractfolders/extractMessageContacts'));
-	// exportData('messageconversations', require('./extractfolders/extractMessageConversations'));
+	exportData('messagecontacts', require('./extractfolders/extractMessageContacts'));
+	exportData('messageconversations', require('./extractfolders/extractMessageConversations'));
 
-	// // Simulated Universe
-	// exportData('sucurios', require('./extractfolders/extractSUCurios'));
-	// exportData('sublessings', require('./extractfolders/extractSUBlessings'));
-	// exportData('suabilities', require('./extractfolders/extractSUAbilities'));
+	// Simulated Universe
+	exportData('sucurios', require('./extractfolders/extractSUCurios'));
+	exportData('sublessings', require('./extractfolders/extractSUBlessings'));
+	exportData('suabilities', require('./extractfolders/extractSUAbilities'));
 }
