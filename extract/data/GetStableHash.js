ca136570bed725690c22b6deec5f9b3b144d9390
theBowja <theBowja@users.noreply.github.com>
2025-02-28 16:30:47 -0500

    Update GetStableHash.js

--- extract/data/GetStableHash.js
+++ extract/data/GetStableHash.js
@@ -1,20 +1,11 @@
+const XXH = require('xxhashjs');
+
 function GetStableHash(str = "") {
-    var hash1 = 5381|0;
-    var hash2 = hash1|0;
-
-    for (let i = 0; i < str.length; i += 2) {
-        hash1 = ((hash1 << 5) + hash1) ^ str.charCodeAt(i);
-        if (i === str.length - 1 || str[i + 1] === '\0')
-            break;
-        hash2 = ((hash2 << 5) + hash2) ^ str.charCodeAt(i + 1);
-
-    }
-
-    return (hash1 + Math.imul(hash2, 1566083941)) | 0;
+    return XXH.h64(str, 0x0).toString();
 }
 
 // console.log(GetStableHash("AvatarRankDesc_100101"));
 
-//-1828743928
+// 6856326848005983588
 
 module.exports = GetStableHash;
