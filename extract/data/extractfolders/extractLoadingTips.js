368a1faf59398fe9a6ef7851c0e204bf9c73469c
theBowja <theBowja@users.noreply.github.com>
2025-04-07 15:07:37 -0400

    Update extractLoadingTips.js

--- extract/data/extractfolders/extractLoadingTips.js
+++ extract/data/extractfolders/extractLoadingTips.js
@@ -12,6 +12,10 @@ function collate(langCode) {
 
 		data.Name = language[obj.TitleTextmapID.Hash];
 
+		if (language[obj.DescTextmapID.Hash] === undefined) {
+			console.log(`No language mapping for LoadingDesc Id: ${id} DescTextmapID.Hash: ${obj.DescTextmapID.Hash}`);
+			return accum;
+		}
 		data.Description = language[obj.DescTextmapID.Hash].replaceAll('\\n', '\n');
 
 		accum[filename] = data;
